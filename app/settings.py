0bc655ea13d38b60101fb217a99c0a54cc76f955
s4ntifdz <santiago.fernandez.utn@gmail.com>
2023-09-19 12:52:51 -0300

    mejora visual del crispyforms con boostrap, todavia no carga los estaticos

--- app/settings.py
+++ app/settings.py
@@ -265,6 +265,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -282,3 +283,4 @@ from django.core.files.storage import FileSystemStorage
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- login/views.py
+++ login/views.py
@@ -8,15 +8,17 @@ from django.template import loaders
 class LoginView(View):
     def get(self, request):
         form = AuthenticationForm()
-        return render(request, 'registration/login.html', {'login_form': form})
-
+        return render(request, 'registration/login.html', {'login_form': form}) 
+        #{'login_form': form} es un diccionario que se pasa a la plantilla login.html, 
+        # para que se pueda acceder a la variable form desde la plantilla.
     def post(self, request):
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
-            if user is not None:
+            if user is not None: 
+            
                 login(request, user)
                 #TODO SE MUESTRA EN EL ADMIN messages.info(request, f"You are now logged in as {username}.")
                 return redirect('base.html')  
