a9d3b9a707fddd2acc85de9a9adafc8ca6aa1b2b
s4ntifdz <santiago.fernandez.utn@gmail.com>
2023-09-29 17:36:48 -0300

    Se agrego funcion vaciar carrito falta mejorar el eliminar articulo no carga total price

--- cart/models.py
+++ cart/models.py
@@ -6,28 +6,72 @@
 #     client = models.ForeignKey(ClientModel, on_delete=models.CASCADE)
 #     stock = models.ForeignKey(StockProduct, on_delete=models.CASCADE)
 #     qt = models.PositiveIntegerField(default=1)
-
+#.....................................................................
 from django.db import models
 from clients.models import ClientModel
 from stock.models import StockProduct
+
+# class Cart(models.Model):
+#     user = models.ForeignKey(ClientModel, on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     update_at = models.DateTimeField(null=True)
+#     def __str__(self):
+#         return f"Carrito de {self.user.username}"
+
+# class CartItem(models.Model):
+#     cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
+#     product = models.ForeignKey(StockProduct, on_delete=models.CASCADE)
+#     quantity = models.PositiveIntegerField(default=1)
+#     @property
+#     def total_price(self): 
+#         return self.quantity * self.product.price
+#     def __str__(self):
+#         return f"{self.quantity} x {self.product.name_product} en {self.cart}"
+
+#     MI DUDA ES SI TENGO QUE CREAR UN MODELO "PADRE" QUE ACUMULE VARIOS 
+#     MODELOS "HIJOS", LOS HIJOS ESTAN INTEGRADOS POR UN PRODUCTO, PRECIO Y CANTIDAD
+#     Y EL MODELO PADRE SOLO SUMA LOS PRECIOS PARA UN PRECIO TOTAL DE COMPRA
+
 
 class Cart(models.Model):
     user = models.ForeignKey(ClientModel, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(null=True)
+
     def __str__(self):
         return f"Carrito de {self.user.username}"
 
+    def update_cart_total(self):
+        cart_items = self.cartitem_set.all()
+        total_quantity = 0
+        total_price = 0
+
+        for item in cart_items:
+            total_quantity += item.quantity
+            total_price += item.total_price
+
+        self.total_quantity = total_quantity
+        self.total_price = total_price
+        self.save()
+    def clear_cart(self):
+        self.cartitem_set.all().delete()
+        self.update_cart_total()
+        
+        
+
 class CartItem(models.Model):
-    cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)  # Relación con el carrito
     product = models.ForeignKey(StockProduct, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+
     @property
-    def total_price(self): 
+    def total_price(self):
         return self.quantity * self.product.price
+
     def __str__(self):
         return f"{self.quantity} x {self.product.name_product} en {self.cart}"
 
-#     MI DUDA ES SI TENGO QUE CREAR UN MODELO "PADRE" QUE ACUMULE VARIOS 
-#     MODELOS "HIJOS", LOS HIJOS ESTAN INTEGRADOS POR UN PRODUCTO, PRECIO Y CANTIDAD
-#     Y EL MODELO PADRE SOLO SUMA LOS PRECIOS PARA UN PRECIO TOTAL DE COMPRA
+    def save(self, *args, **kwargs):
+        # Actualizar el carrito después de guardar o actualizar un CartItem
+        self.cart.update_cart_total()
+        super().save(*args, **kwargs)
--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,3 @@
-from django.shortcuts import render
-from django.views import View
-from django.http import HttpResponse
-from django.template import loader
-from django.contrib.auth.mixins import LoginRequiredMixin
-from stock.models import StockProduct as StockModel
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views import View
@@ -13,37 +7,41 @@ from django.http import HttpResponse
 
 class CartView(View):
     def get(self, request):
-        template = loader.get_template("cart.html")
-        context = {}
-        return HttpResponse(template.render(context, request))
+        
+        cart, _ = Cart.objects.get_or_create(user=request.user)
+        
+        context = {
+            'cart': cart,
+        }
+        return render(request, "cart.html", context)
+    def clear_cart(self, request):
+        cart, _ = Cart.objects.get_or_create(user=request.user)
+        cart.cartitem_set.all().delete()
+        return redirect('cart')
     
-    def post(self, request):
-        if not request.user.is_authenticated: 
-            messages.error(request, 'Debes iniciar sesión para agregar elementos al carrito.')
-            return redirect('login')  
-
+    def post(self, request): 
+        if 'clear_cart' in request.POST:
+            return self.clear_cart(request)
 
         product_id = int(request.POST.get('id'))
-        quantity = int(request.POST.get('qt', 3))  
-        product = StockProduct.objects.get(pk=product_id) 
-        
-        cart, _ = Cart.objects.get_or_create(user=request.user) 
-        # _ sirve para ignorar el segundo valor que retorna get_or_create, 
-        # que es un booleano que indica si se creó o no el objeto
-
-        cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
-        if not created:
-            cart_item.quantity += quantity
-            cart_item.save()
+        quantity = int(request.POST.get('qt', 1))
+        product = StockProduct.objects.get(pk=product_id)
+        if product.quantity_stock >= quantity:
+            product.quantity_stock -= quantity
+            product.save()
         else:
-            cart_item.quantity = quantity
-            cart_item.save()
-        
-        context ={
-            'cart': cart,
-            'cart_item': cart_item,
-            'product': product,
-            'quantity': quantity,
-        }
-        template = loader.get_template("cart.html")
-        return HttpResponse(template.render(context, request))
+            messages.error(request, 'No hay suficiente stock disponible para este producto.')
+            return redirect('/publication/'+str(product_id)) 
+
+        product = StockProduct.objects.get(pk=product_id) 
+        cart, _ = Cart.objects.get_or_create(user=request.user)
+        cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+        cart_item.quantity += quantity
+        cart_item.save()
+        cart.update_cart_total()
+        cart.save()
+        return redirect('cart') 
+    
+
+#TODO falta validar stock
+
